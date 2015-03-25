988aae786ddfd1626d330e9c7d952a1dc200df99
Joe Jordan <joe.jordan@imperial.ac.uk>
2015-03-25 22:20:18 +0000

    porting over token type decisions

--- parse.py
+++ parse.py
@@ -99,4 +99,110 @@ def parse(inp):
     return output_list
 
 
+class NIL(object):
+    def __repr__(self):
+        return 'NIL'
+
+def crepr(s):
+    assert isinstance(s, str), "crepr is for strings, i.e. str() instances."
+    prepr = repr(s)
+    if prepr[:3] == '"""':
+        # anything goes, need to manually escape everything.
+        raise NotImplementedError("not implemented for this repr type.")
+    elif prepr[0] == '"':
+        return prepr
+    else:
+        assert prepr[0] == "'", "unknown repr output from python."
+        return '"' + prepr[1:-1].replace("\\'", "'").replace('"', '\\"') + '"'
+
+
+def lrepr(s):
+    if isinstance(s, str):
+        return crepr(s)
+    return repr(s)
+
+
+class MinimalispType(object):
+    pass
+
+class Symbol(MinimalispType):
+    """only stores its text representation as a python string in upper case
+    - can therefore be used as a dict key."""
+    def __init__(self, s):
+        self.s = s.upper()
+
+    def __eq__(self, other):
+        if other is not Symbol:
+            return False
+        if self.s == other.s:
+            return True
+
+    def __repr__(self):
+        return self.s
+
+    def __hash__(self):
+        return hash(self.s)
+
+
+class Value(MinimalispType):
+    """stored simply as the relevant python type (string, int or float)."""
+    def __init__(self, v):
+        self.v = eval(v)
+    
+    def __repr__(self):
+        if isinstance(self.v, str):
+            return crepr(self.v)
+        else:
+            return repr(self.v)
+
+
+PAIR_SEPARATOR = '.'
+
+class PAIR_LITERAL(object):
+    pass
+
+NUMERIC = "0123456789"
+BEGIN_HEXANUMERIC = "#"
+HEXANUMERIC = NUMERIC + "ABCDEF"
+STRINGY = '"'
+
+DUBIOUS = "+-"
+ALLOWED_IN_NUMERIC = NUMERIC + DUBIOUS + "eE."
+
+def parse_token(t):
+    char1 = t[0]
+    # shift the decision onto the second char.
+    if char1 in DUBIOUS:
+        # check ahead to see what type we're dealing with.
+        if len(t) == 1:
+            # this is a single + or - as a symbol. do nothing.
+            pass
+        elif t[1] in NUMERIC:
+            # if numeric, just set char1 so that the next section categorises properly.
+            char1 = t[1]
+    
+    if char1 in NUMERIC:
+        assert all([c in ALLOWED_IN_NUMERIC for c in t]), "token %s contains invalid characters to be a numeric literal." % t
+        v = Value(t)
+    elif char1 in STRINGY:
+
+        v = Value(t)
+    elif char1 in BEGIN_HEXANUMERIC:
+        assert all([c in HEXANUMERIC for c in t[1:]]), "token %s contains invalid characters for a hexadecimal literal." % t
+        t = t.replace('#', '0x')
+        v = Value(t)
+    elif char1 in PAIR_SEPARATOR:
+        assert len(t) == 1, "token %s is invalid: cannot use . at the start of a symbol or literal." % t
+        v = PAIR_LITERAL
+    else:
+        if t.upper() == "NIL":
+            v = NIL()
+        else:
+            v = Symbol(t)
+    return v
+
+
+def parse_program(p):
+
+
 
