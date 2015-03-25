8cb5fc21dbf1f7835c1f8bcf1e3cd492b0fb9838
Joe Jordan <joe.jordan@imperial.ac.uk>
2015-03-25 20:34:02 +0000

    improved parse which handles comments and strings

--- parse.py
+++ parse.py
@@ -1,3 +1,5 @@
+import re
+
 class sexpr(list):
     def __init__(self, *args, **kwargs):
         self.quoted = False
@@ -9,8 +11,63 @@ class sexpr(list):
             return "'" + outer
         return outer
 
+def remove_comments(inp):
+    commentless_lines = []
+    
+    # kill comment characters ; onwards, when not inside a string.
+    for l in inp.split("\n"):
+        splits = l.split(';')
+        if len(splits) == 1:
+            commentless_lines.append(l)
+            continue
+        
+        previous_string = splits.pop(0)
+        
+        while len(re.findall(r'(?<!\\)"', previous_string)) % 2 != 0:
+            previous_string += ';' + splits.pop()
+        
+        commentless_lines.append(previous_string)
+    return "\n".join(commentless_lines)
+
+
+def clever_split(inp):
+    commentless_inp = remove_comments(inp)
+    
+    # rather than splitting on all whitespace, we want to split only on 
+    # whitespace not inside a string.
+    # strings cannot contain newlines, so start by splitting on those:
+    lines = commentless_inp.split("\n")
+    tokens = []
+    for i, l in enumerate(lines):
+        splits = [s for s in l.split('"')]
+
+        # if there were no "s in this line, split the whole thing by whitespace:
+        if len(splits) == 1:
+            tokens.extend(l.split())
+            continue
+        
+        # first " can't be escaped.
+        tokens.extend(splits.pop(0).split())
+
+        while splits:
+            this_string = splits.pop(0)
+            # while the quotes are escaped:
+            while this_string[-1] == "\\":
+                assert len(splits) > 0, "string on line %d is not terminated correctly." % i
+                this_string = this_string[:-1] + '"' + splits.pop(0)
+            # we've got to the end of this string, add it as its own token:
+            tokens.append('"' + this_string + '"')
+
+            # the next tokens are not strings, so just extend them (if any):
+            if splits:
+                tokens.extend(splits.pop(0).split())
+
+    return tokens
+
+    
+
 def parse(inp):
-    toks = inp.split()
+    toks = clever_split(inp)
     parents = {}
     output_list = sexpr()
     current_list = output_list
