93af9a458da85a28a9ad98821009b7dc289b26dc
Joe Jordan <joe@joe-jordan.co.uk>
2016-01-31 22:09:32 +0000

    refactored unit tests to de-duplicate with macros

--- unit_tests/test_values.c
+++ unit_tests/test_values.c
@@ -5,37 +5,48 @@
 
 mnl_pool* memory_pool = NULL;
 
+#define test_strings(name, fixtures, test) \
+  do { \
+    char *start, *end; \
+    start = fixtures; \
+    end = start; \
+    while (*start != '\0') { \
+      while (*end != '\n') { \
+        ++end; \
+      } \
+      *end = '\0'; \
+      printf("testing string %s in the %s parser...", start, name); \
+      test(start); \
+      printf("   success!\n"); \
+      ++end; \
+      start = end; \
+    } \
+  } while(0)
+
+#define parse_int_success(string) \
+  do { \
+    mnl_object* i = mnl_integer_from_string(memory_pool, string); \
+    ck_assert_ptr_ne(i, NULL); \
+    ck_assert_uint_eq(i->type, MNL_INTEGER); \
+    int_release(memory_pool, i); \
+  } while(0)
+
 START_TEST(test_mnl_integer_from_string) {
   char* valid_integers = strdup("1\n123\n-456\n#deadbeef\n0700\n");
-  char *start, *end;
-  start = valid_integers;
-  end = start;
-
-  while (*start != '\0') {
-    while (*end != '\n') {
-      ++end;
-    }
-    *end = '\0';
-
-    printf("testing string \"%s\" in the int parser...", start);
-
-    /* use start as a string: */
-    mnl_object* a = mnl_integer_from_string(memory_pool, start);
-    ck_assert_ptr_ne(a, NULL);
-    ck_assert_uint_eq(a->type, MNL_INTEGER);
-
-    /* free the RAM that was allocated by this test: */
-    int_release(memory_pool, a);
-
-    printf("   success!\n");
-
-    /* reset for the next string.*/
-    ++end;
-    start = end;
-  }
+  test_strings("int", valid_integers, parse_int_success);
   free(valid_integers);
 }
 END_TEST
+
+#define parse_int_compare_correct(string) \
+  do { \
+    mnl_object* i = mnl_integer_from_string(memory_pool, string); \
+    ck_assert_ptr_ne(i, NULL); \
+    ck_assert_uint_eq(i->type, MNL_INTEGER); \
+    ck_assert_int_eq(mpz_cmp(*(mpz_t*)(i->value), correct_value), 0); \
+    int_release(memory_pool, i); \
+  } while(0)
+
 
 START_TEST(test_mnl_integer_from_string_tricky) {
   /* These values all equal (decimal) -302. */
@@ -43,94 +54,36 @@ START_TEST(test_mnl_integer_from_string_tricky) {
   mpz_t correct_value;
   mpz_init_set_si(correct_value, -302);
 
-  char *start, *end;
-  start = valid_integers;
-  end = start;
+  test_strings("int", valid_integers, parse_int_compare_correct);
 
-  while (*start != '\0') {
-    while (*end != '\n') {
-      ++end;
-    }
-    *end = '\0';
-
-    printf("testing string \"%s\" in the int parser...", start);
-
-    /* use start as a string: */
-    mnl_object* a = mnl_integer_from_string(memory_pool, start);
-    ck_assert_ptr_ne(a, NULL);
-    ck_assert_uint_eq(a->type, MNL_INTEGER);
-    ck_assert_int_eq(mpz_cmp(*(mpz_t*)(a->value), correct_value), 0);
-
-    /* free the RAM that was allocated by this test: */
-    int_release(memory_pool, a);
-
-    printf("   success!\n");
-
-    /* reset for the next string.*/
-    ++end;
-    start = end;
-  }
-  mpz_clear(correct_value);
   free(valid_integers);
 }
 END_TEST
 
+#define parse_int_failure(string) \
+  do { \
+    mnl_object* n = mnl_integer_from_string(memory_pool, start); \
+    ck_assert_ptr_eq(n, NULL); \
+  } while(0)
+
 START_TEST(test_mnl_integer_from_string_bad_strings) {
   char* invalid_integers = strdup("1a\n1u23\n-45.6\n#degaef\n0800\n");
-  char *start, *end;
-  start = invalid_integers;
-  end = start;
-
-  while (*start != '\0') {
-    while (*end != '\n') {
-      ++end;
-    }
-    *end = '\0';
-
-    printf("testing string \"%s\" in the int parser...", start);
-
-    /* use start as a string: */
-    mnl_object* a = mnl_integer_from_string(memory_pool, start);
-    ck_assert_ptr_eq(a, NULL);
-
-    printf("   success!\n");
-
-    /* reset for the next string.*/
-    ++end;
-    start = end;
-  }
+  test_strings("int", invalid_integers, parse_int_failure);
   free(invalid_integers);
 }
 END_TEST
 
+#define parse_real_success(string) \
+  do { \
+    mnl_object* a = mnl_real_from_string(memory_pool, start); \
+    ck_assert_ptr_ne(a, NULL); \
+    ck_assert_uint_eq(a->type, MNL_REAL); \
+  } while(0)
 
 START_TEST(test_mnl_real_from_string) {
-  char* valid_floats = strdup("1.\n.1\n+.1\n-1.\n1.1\n+7.7438e-4\n");
-  char *start, *end;
-  start = valid_floats;
-  end = start;
-
-  while (*start != '\0') {
-    while (*end != '\n') {
-      ++end;
-    }
-    *end = '\0';
-
-    printf("testing string \"%s\" in the float parser...", start);
-
-    /* use start as a string: */
-    mnl_object* a = mnl_real_from_string(memory_pool, start);
-    ck_assert_ptr_ne(a, NULL);
-    ck_assert_uint_eq(a->type, MNL_REAL);
-
-
-    printf("   success!\n");
-
-    /* reset for the next string.*/
-    ++end;
-    start = end;
-  }
-  free(valid_floats);
+  char* valid_reals = strdup("1.\n.1\n+.1\n-1.\n1.1\n+7.7438e-4\n");
+  test_strings("real", valid_reals, parse_real_success);
+  free(valid_reals);
 }
 END_TEST
 
